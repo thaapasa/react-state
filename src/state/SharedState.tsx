e4073ef749c0d32055f580303caf81a77d61a324
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2021-11-19 23:06:40 +0200

    Clean up code

--- src/state/SharedState.tsx
+++ src/state/SharedState.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 
-type NotifyFun<T> = (newValue: T) => void
+/**
+ * State setter
+ */
+type StateSetFunction<T> = (newValue: T) => void
 
+/**
+ * The return type of useState(); identical to React.useState()
+ */
 type StateResult<T> = [T, (newValue: T) => void]
 
-interface StateManager<V> {
-  current: V
-  useState: () => StateResult<V>
-}
+/**
+ * Custom useState function signature
+ */
+type CustomUseState<V> = () => StateResult<V>
 
-function createSharedState<T>(prototype: T): { [K in keyof T]: StateManager<T[K]>} {
-  const state: { [K in keyof T]: StateManager<T[K]>} = {} as any
+function createSharedState<T>(prototype: T): { [K in keyof T]: CustomUseState<T[K]>} {
+  const state: { [K in keyof T]: CustomUseState<T[K]>} = {} as any
   Object.keys(prototype).forEach((k: any) => (state as any)[k] = createStateManager((prototype as any)[k]))
   return state
 }
 
-function createStateManager<V>(initialValue: V): StateManager<V> {
+function createStateManager<V>(initialValue: V): CustomUseState<V> {
   let value = initialValue
-  let listeners: NotifyFun<V>[] = []
+  let listeners: StateSetFunction<V>[] = []
   const propagate = (v: V) => {
     // Update actual state value
     value = v
@@ -25,20 +31,17 @@ function createStateManager<V>(initialValue: V): StateManager<V> {
     listeners.forEach(l => l(v))
   }
 
-  return {
-    current: initialValue,
-    useState: () => {
-      const [, setVal] = React.useState(value)
-      React.useEffect(() => {
-        // Track this new internal state value
-        listeners.push(setVal)
-        return () => {
-          // Remove internal state tracking when the call to this custom useState is unmounted
-          listeners.splice(listeners.findIndex(l => l === setVal), 1)
-        }
-      }, [setVal])
-      return [value, propagate]
-    }
+  return () => {
+    const [, setVal] = React.useState(value)
+    React.useEffect(() => {
+      // Track this new internal state value
+      listeners.push(setVal)
+      return () => {
+        // Remove internal state tracking when the call to this custom useState is unmounted
+        listeners.splice(listeners.findIndex(l => l === setVal), 1)
+      }
+    }, [setVal])
+    return [value, propagate]
   }
 }
 
@@ -47,10 +50,14 @@ export function createState<T>(initialState: T) {
   const state = createSharedState(initialState)
   const Context = React.createContext(state)
 
-  const Provider: React.FC<{}> = ({children}) => <Context.Provider value={state}>{children}</Context.Provider>
+  const Provider: React.FC<{}> = ({children}) => (
+    <Context.Provider value={state}>
+      {children}
+    </Context.Provider>
+  )
 
   return {
     Provider,
-    useState: (name: keyof T) => React.useContext(Context)[name].useState()
+    useState: (name: keyof T) => React.useContext(Context)[name]()
   }
 }
